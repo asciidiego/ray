251a064e43906f54cd9bade6c6691cc50cb83c9d
diego <diego.vincent.rodriguez@gmail.com>
2023-03-07 03:28:52 -0800

    basic progress

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
+
 	// Image
 	const int image_width 	= 256;
 	const int image_height	= 256;
@@ -9,6 +10,8 @@ int main() {
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int i = image_height - 1; i >= 0; --i) {
+		// Scanline show progress
+		std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
 		for (int j = 0; j < image_width; ++j) {
             auto r = double(j) / (image_width - 1);
             auto g = double(i) / (image_height - 1);
@@ -21,4 +24,6 @@ int main() {
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
 		}
 	}
+
+	std::cerr << "\nDone.\n";
 }
