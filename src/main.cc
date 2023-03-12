a6dd3a3dca17296156eda414393dd0e581221525
diego <diego.vincent.rodriguez@gmail.com>
2023-03-11 18:18:34 -0800

    feat: sending rays into the scene

--- src/main.cc
+++ src/main.cc
@@ -1,27 +1,47 @@
 #include "vec3.h"
 #include "color.h"
+#include "ray.h"
 
 #include <iostream>
+
+color ray_color(const ray& r) {
+	vec3 unit_direction = normalize(r.direction());
+
+	// Create a blueish gradient that depends on Y-axis position of ray
+	auto t = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 
 int main() {
 
 	// Image
-	const int image_width 	= 256;
-	const int image_height	= 256;
+	const auto aspect_ratio = 16.0 / 9.0;
+	const int image_width 	= 512;
+	const int image_height	= static_cast<int>(image_width / aspect_ratio);
+
+	// Camera
+	auto viewport_height 	= 2.0;
+	auto viewport_width  	= aspect_ratio * viewport_height;
+	auto focal_length 		= 1.0;
+
+	auto origin 			= point3(0, 0, 0);
+	auto horizontal			= vec3(viewport_width, 0, 0);
+	auto vertical			= vec3(0, viewport_height, 0);
+	auto lower_left_corner	= origin - (horizontal / 2) - (vertical / 2) - vec3(0, 0, focal_length);
 
 	// Render
+
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
 	for (int i = image_height - 1; i >= 0; --i) {
-		// Scanline show progress
 		std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
 		for (int j = 0; j < image_width; ++j) {
-            auto r = double(j) / (image_width - 1);
-            auto g = double(i) / (image_height - 1);
-            auto b = 0.25;
+			auto u = double(j) / (image_width - 1);
+			auto v = double(i) / (image_height - 1);
 
-			color pixel_color(r, g, b);
+			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+			color pixel_color(ray_color(r));
 			write_color(std::cout, pixel_color);
 		}
 	}
