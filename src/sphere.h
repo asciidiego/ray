f048c5fb32e7721d59301b74a2b4bbf41c4eb1f9
diego <diego.vincent.rodriguez@gmail.com>
2023-03-14 04:06:55 -0700

    feat: front-face v back-face

--- src/sphere.h
+++ src/sphere.h
@@ -6,42 +6,43 @@
 
 class sphere : public hittable {
 public:
-	sphere() {}
-	sphere(point3 cen, double r) : center(cen), radius(r) {};
+  sphere() {}
+  sphere(point3 cen, double r) : center(cen), radius(r){};
 
-	virtual bool hit(
-		const ray& r, double t_min, double t_max, hit_record& rec) const override;
+  virtual bool hit(const ray &r, double t_min, double t_max,
+                   hit_record &rec) const override;
 
 public:
-	point3 center;
-	double radius;
+  point3 center;
+  double radius;
 };
 
-bool sphere::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
-	vec3 oc = r.origin() - center;
-	auto a 	= r.direction().length_squared();
-	auto half_b = dot(r.direction(), oc);
-	auto c	= dot(oc, oc) - radius * radius;
+bool sphere::hit(const ray &r, double t_min, double t_max,
+                 hit_record &rec) const {
+  vec3 oc = r.origin() - center;
+  auto a = r.direction().length_squared();
+  auto half_b = dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
 
-	auto discriminant = half_b*half_b - a*c;
-	if (discriminant < 0) return false;
-	auto sqrtd = sqrt(discriminant);
+  auto discriminant = half_b * half_b - a * c;
+  if (discriminant < 0)
+    return false;
+  auto sqrtd = sqrt(discriminant);
 
-	// Find the nearest root within `t` range
-	auto root = (-half_b - sqrtd ) / a;
-	if (root < t_min || root > t_max) {
-		root = (-half_b + sqrtd) / a;
-		if (root < t_min || root > t_max)
-			return false;
-	}
+  // Find the nearest root within `t` range
+  auto root = (-half_b - sqrtd) / a;
+  if (root < t_min || root > t_max) {
+    root = (-half_b + sqrtd) / a;
+    if (root < t_min || root > t_max)
+      return false;
+  }
 
-	// There was a hit; store it
-	// TODO?: more than one hit (case where equation has two solutions)
-	rec.t = root;
-	rec.p = r.at(rec.t);
-	rec.normal = (rec.p - center) / radius;
+  rec.t = root;
+  rec.p = r.at(rec.t);
+  vec3 outward_normal = (rec.p - center) / radius;
+  rec.set_face_normal(r, outward_normal);
 
-	return true;
+  return true;
 }
 
 #endif
