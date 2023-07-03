1707bd018e230795fa3e1ecb001a242154e09955
diego <d@krea.ai>
2023-07-03 01:42:27 -0700

    feat: scene with metal spheres
    ref: https://raytracing.github.io/books/RayTracingInOneWeekend.html#metal/ascenewithmetalspheres

--- src/main.cc
+++ src/main.cc
@@ -46,20 +46,24 @@ color ray_color(const ray &r, const hittable &world, int depth) {
 int main() {
 
   // Image
-  const auto aspect_ratio = 2.0 / 1;
+  const auto aspect_ratio = 2.0 / 1.0;
   const int image_width = 640;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
-  const int samples_per_pixel = 4;
+  const int samples_per_pixel = 16;
   const int max_depth = 50;
 
   // World
   hittable_list world;
 
-  auto material_center = make_shared<lambertian>(color(0.8, .8, .0));
   auto material_ground = make_shared<lambertian>(color(0.7, .3, .3));
+  auto material_center = make_shared<lambertian>(color(0.8, .8, .0));
+  auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
+  auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
 
-  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, material_center));
-  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, material_ground));
+  world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0),  100, material_ground));
+  world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),  0.5, material_center));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  0.5, material_left));
+  world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),  0.5, material_right));
 
   // Camera
   camera cam;
